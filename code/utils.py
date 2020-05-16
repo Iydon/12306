56ce82b084425e3e6d20d20d5dfb6a77f6de9ce1
iydon <11711217@mail.sustech.edu.cn>
2020-05-16 23:15:08 +0800

    update: add some get class methods

--- code/utils.py
+++ code/utils.py
@@ -36,15 +36,100 @@ class get:
     '''
     @classmethod
     def admin(cls, name):
-        return cls._(Admins, Admins.admin_name==name)
+        '''
+        Argument:
+            - name: str
+
+        Return:
+            - Admins
+        '''
+        return cls._by(Admins, admin_name=name)
 
     @classmethod
     def user(cls, id_card):
-        return cls._(Users, Users.id_card_num==id_card)
+        '''
+        Argument:
+            - id_card: str
+
+        Return:
+            - Users
+        '''
+        return cls._by(Users, id_card_num=id_card)
 
     @classmethod
-    def _(cls, model, condition):
-        return session.query(model).filter(condition).first()
+    def cities(cls):
+        '''
+        Return:
+            - list[str]
+        '''
+        return cls._compress(session.query(Cities.city_name))
+
+    @classmethod
+    def province(cls):
+        '''
+        Return:
+            - list[str]
+        '''
+        return cls._compress(session.query(Cities.province.distinct()))
+
+    @classmethod
+    def stations(cls):
+        '''
+        Return:
+            - list[str]
+        '''
+        return cls._compress(session.query(Stations.station_name).all())
+
+    @classmethod
+    def cities_by_province(cls, province):
+        '''
+        Argument:
+            - province: str
+
+        Return:
+            - list[str]
+        '''
+        return cls._compress(cls._by(Cities.city_name, province=province, all=True))
+
+    @classmethod
+    def stations_by_city(cls, city_name):
+        '''
+        Argument:
+            - city_name: str
+
+        Return:
+            - list[str]
+        '''
+        city_id = cls._by(Cities.city_id, city_name=city_name)
+        return cls._compress(cls._by(Stations.station_name, city_id=city_id, all=True))
+
+    @classmethod
+    def train_ids_by_stations(cls, from_station, to_station):
+        '''
+        Argument:
+            - from_station: str
+            - to_station: str
+
+        Return:
+            - list[str]
+        '''
+        from_station_id = cls._by(Stations.station_id, station_name=from_station)
+        to_station_id = cls._by(Stations.station_id, station_name=to_station)
+        columns = Journeys.train_id, Journeys.station_index
+        from_train_ids = cls._by(*columns, station_id=from_station_id, all=True)
+        to_train_ids = cls._by(*columns, station_id=to_station_id, all=True)
+        # find the train_id where from_station and to_station in it
+        data = dict(from_train_ids)
+        return [i for i, j in to_train_ids if (i in data and j>data[i])]
+
+    @classmethod
+    def _compress(cls, data):
+        return [d[0] for d in data]
+
+    @classmethod
+    def _by(cls, *models, all=False, **condition):
+        query = session.query(*models).filter_by(**condition)
+        return query.all() if all else query.first()
 
 
 class registered:
